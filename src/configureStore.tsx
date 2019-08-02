24b60530e078903ceaafe39d83a81d469f072325
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-08-02 11:42:46 -0600

    Fixed Duplicate key bug on table in dashboard.

--- src/configureStore.tsx
+++ src/configureStore.tsx
@@ -1,24 +1,22 @@
-import { createStore, applyMiddleware, compose, Store } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { routerMiddleware } from "connected-react-router";
 import { createBrowserHistory } from "history";
 import createRootReducer from "./reducers";
 import { History } from "history"
 
-// @ts-ignore
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const composeEnhancers = compose;
 
 export const history: History = createBrowserHistory();
 
 function configureStore( preloadedState ) {
-  const store: Store = createStore( createRootReducer( history ),
+  return createStore( createRootReducer( history ),
     preloadedState,
     composeEnhancers( applyMiddleware( routerMiddleware( history ),
       thunk
     ), ),
   );
-  
-  return store;
 }
 
 const initialState = {};
