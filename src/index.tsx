ec45b19e5d313205fc83304789f8859ef422f8ac
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-08-02 11:43:17 -0600

    Fixed Duplicate key bug on table in dashboard.

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,13 @@ import "antd/dist/antd.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Router } from "react-router-dom";
-import LogRocket from "logrocket";
-import { Provider } from "react-redux";
-import setupLogRocketReact from "logrocket-react";
+import { Provider } from "react-redux"
 import store, { history } from "./configureStore";
 
-setupLogRocketReact( LogRocket );
-
 ReactDOM.render( <Provider store={ store }>
-    <Router history={ history }>
-        <App/>
-    </Router>
+  <Router history={ history }>
+    <App/>
+  </Router>
 </Provider>, document.getElementById( "root" ) );
 
 // If you want your app to work offline and load faster, you can change
